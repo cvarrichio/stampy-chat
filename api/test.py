bbc14c7c434e4521f79687a68191348905711546
Craig Varrichio <canthony427@gmail.com>
2023-07-07 08:00:20 -0500

    Fully functional prompt/API testing script.

--- api/test.py
+++ api/test.py
@@ -1,7 +1,10 @@
-
 import aiohttp
 import asyncio
 import logging
+import csv
+import datetime
+
+logging.basicConfig(level=logging.DEBUG)
 
 async def send_to_api(question, session):
     url = f'https://chat.stampy.ai:8443/chat/{question}'
@@ -15,46 +18,70 @@ async def send_to_api(question, session):
                 logging.debug(f"API response: {response_text}")
                 if resp.status != 200:
                     logging.info(f"Error on API request: {await resp.json(content_type=None)}")
-                    return None
-                result = await resp.json(content_type=None)
-                from chat import construct_prompt
-                result['prompt'] = construct_prompt(question,[],[])
-                return result
+                return await resp.json(content_type=None),question if resp.status == 200 else None
         except (Exception) as e:
                 logging.info(f"Error on API request, retrying: {e}")
                 retry_count += 1
                 if retry_count >= RETRIES:
                     logging.info(f"Unexpected error on API request: {e}")
-                    raise  # Re-raise any other exceptions
+                    return None
                 else:
-                    await asyncio.sleep(2)  # Use asyncio.sleep instead of time.sleep
+                    await asyncio.sleep(2)  
 
         finally:
             await asyncio.sleep(0.1) 
+
 
 async def call_api(questions):
     async with aiohttp.ClientSession() as session:
         tasks = [send_to_api(question, session) for question in questions]
         results = await asyncio.gather(*tasks)
-    return results
+
+    from chat import construct_prompt
+    for result, question in results:
+        if result is not None:  # Check if the result is not None
+            result['prompt'] = construct_prompt(question,[],[])
+            result['query'] = question
+
+    return [result for result, question in results]
 
 import nest_asyncio
 nest_asyncio.apply()
 
-# Run the function
-import sys
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
-questions = [
-    "Can AI take over the world?",
-    "Can we just turn off AI if it becomes dangerous?",
-    "Can AI become conscious or have feelings?",
-    "Will AI replace all human jobs?",
-    "Can AI be ethical or moral?",
-    "Will you kill us all?",
-    "What is AI alignment, and why is it important?",
-    "What are some potential risks associated with advanced AI, and how can they be mitigated?",
-    "Can AI be programmed to behave ethically, and how can we ensure that it does?",
-    "How can we ensure that AI systems are transparent and explainable in their decision-making processes?",
-    "What is the difference between narrow and general AI, and how does this impact safety and alignment?"
-]
-results = asyncio.run(call_api(questions))
+def format_citations(citations):
+    formatted_citations = []
+    for key, citation in citations.items():
+        formatted_citation = f"[{key}]{citation['title']} - {citation['author']} - {citation['date']}"
+        formatted_citations.append(formatted_citation)
+    return '\n'.join(formatted_citations)
+
+if __name__ == '__main__':
+
+    # Run the function
+    questions = [
+        "Can AI take over the world?",
+        "Can we just turn off AI if it becomes dangerous?",
+        "Can AI become conscious or have feelings?"
+    ]
+    results = asyncio.run(call_api(questions))
+
+    # Prepare data for CSV
+    csv_data = [['query', 'generated_response', 'citations', 'prompt_template']]
+
+    # Iterate over each response in the list
+    for json_obj in results:
+        # Update the 'citations' field in the JSON object
+        json_obj['formatted_citations'] = format_citations(json_obj['citations'])
+
+        # Extract prompt template from prompt
+        prompt_template = next((item for item in json_obj['prompt'] if item["role"] == "system"), None)['content']
+
+        # Append data to CSV data list
+        csv_data.append([json_obj['query'], json_obj['response'], json_obj['formatted_citations'], prompt_template])
+
+    now = datetime.datetime.now() # current date and time
+    timestamp = now.strftime("%Y%m%d%H%M%S")
+
+    with open(f'prompt_test_results_{timestamp}.csv', 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerows(csv_data)
